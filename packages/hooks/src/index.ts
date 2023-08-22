b441a69a0d2a1244f83a83c9ff8fa66b58855d7f
doooss <ldy9212@gmail.com>
2023-08-22 21:52:08 +0900

    lint

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,5 +1,6 @@
 export { default as useBoolean } from './useBoolean';
 export { default as useDebounceEffect } from './useDebounceEffect';
+export { default as useDevice } from './useDevice';
 export { default as useDidMountedEffect } from './useDidMountedEffect';
 export { default as useEventListener } from './useEventListener';
 export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
@@ -7,5 +8,3 @@ export { default as useLockedBody } from './useLockedBody';
 export { default as useObserver } from './useObserver';
 export { default as useOnClickOutSide } from './useOnClickOutSide';
 export { default as useWindowSize } from './useWindowSize';
-
-export { default as useDevice } from './useDevice'
